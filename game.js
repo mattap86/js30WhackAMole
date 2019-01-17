e6fe0f641196f6da5a5413ef7e0ec326ce6b1552
Matt Tapley <matthew.tapley@hotmail.co.uk>
2019-01-17 15:17:27 +0000

    created function to randomise the hole the mole appears from and another to randomise how long they appear for

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 const holes = document.querySelectorAll('.hole')
 const scoreBoard = document.querySelector('.score')
 const moles = document.querySelectorAll('.mole')
+let lastHole
 
-document.getElementsByClassName('.start').addEventListener('click', function {
+document.getElementsByClassName('.start').addEventListener('click', function() {
     startGame
 })
 
@@ -12,4 +13,18 @@ const startGame = () => {
     score = 0
     peep()
     setTimeout(() => timeUp = true, 10000)
+}
+
+const randomTime = (min, max) => {
+    return Math.random() * (max - min) * min)
+}
+
+const randomHole = holes => {
+    const index = Math.floor(Math.random() * holes.length)
+    const hole = holes[index]
+    if (hole === lastHole) {
+        return randomHole(holes)
+    } 
+    lastHole = hole
+    return hole
 }
