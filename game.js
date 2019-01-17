4f2be03eaa9fc098b4c9e5c2937e2f8ffe610603
Matt Tapley <matthew.tapley@hotmail.co.uk>
2019-01-17 14:32:04 +0000

    created startGame function and added event listener to start button

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
-const holes = = document.querySelectorAll('.hole')
+const holes = document.querySelectorAll('.hole')
 const scoreBoard = document.querySelector('.score')
 const moles = document.querySelectorAll('.mole')
+
+document.getElementsByClassName('.start').addEventListener('click', function {
+    startGame
+})
+
+const startGame = () => {
+    scoreBoard.textContent = 0
+    timeUp = false
+    score = 0
+    peep()
+    setTimeout(() => timeUp = true, 10000)
+}
