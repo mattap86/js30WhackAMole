bf5941f7886cb2b0fe7a67a35a6f563aff64142c
Matt Tapley <matthew.tapley@hotmail.co.uk>
2019-01-18 18:36:13 +0000

    changed score class tag to p from span, removed styling for .score in styles.css, reformatted game.js code to rectify (event listener not a function) error, will remedy on next update

--- game.js
+++ game.js
@@ -1,30 +1,42 @@
-const holes = document.querySelectorAll('.hole')
-const scoreBoard = document.querySelector('.score')
-const moles = document.querySelectorAll('.mole')
-let lastHole
-
-document.getElementsByClassName('.start').addEventListener('click', function() {
-    startGame
-})
-
-const startGame = () => {
-    scoreBoard.textContent = 0
-    timeUp = false
-    score = 0
-    peep()
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        console.log('Ah nah thats the same one bud');
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
     setTimeout(() => timeUp = true, 10000)
 }
-
-const randomTime = (min, max) => {
-    return Math.random() * (max - min) * min)
+function bonk(e) {
+    if (!e.isTrusted) return; // cheater!
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
 }
-
-const randomHole = holes => {
-    const index = Math.floor(Math.random() * holes.length)
-    const hole = holes[index]
-    if (hole === lastHole) {
-        return randomHole(holes)
-    } 
-    lastHole = hole
-    return hole
-}
+moles.forEach(mole => mole.addEventListener('click', bonk));
